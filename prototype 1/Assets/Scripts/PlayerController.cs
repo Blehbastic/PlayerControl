0ee49489235a1466fd4f1f8d8b75964afb369f5a
Nathaniel Rosas <rosasn@roverkids.org>
2023-11-15 13:33:42 -0500

    Add Turn Code

--- prototype 1/Assets/Scripts/PlayerController.cs
+++ prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
